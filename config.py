1b5103b4d0f8d8fbf9d24072bc21484b40227f6b
Israel Tariku <isra3lt3cho@gmail.com>
2026-01-17 17:45:47 -0300

    Add project files and folders

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+BOT_TOKEN = "8339236362:AAHMbjioI3yn1OOFMFcAa41WRycgxK3qoHg"
+
+# Replace with actual IDs
+ADMIN_GROUP_ID = -5177131315   # Telegram group for admins
+CHANNEL_ID = -1003613180715       # Public channel for job postings
